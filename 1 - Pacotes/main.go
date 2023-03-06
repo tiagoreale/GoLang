6f19978e61893cff8f82a72d933aea6fc19e5520
tiagoreale <tiagoreale@live.com>
2023-03-06 17:09:24 -0300

    Main

---
+++ 1 - Pacotes/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+	"modulo/auxiliar"
+)
+
+// Escrever registra uma mensagem na tela
+func main() {
+	fmt.Println("Escrevendo do arquivo main")
+	auxiliar.Escrever()
+}
