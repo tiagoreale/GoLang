0b83a8704243a8c128247e4cf20adab96c379148
tiagoreale <tiagoreale@live.com>
2023-03-07 14:39:58 -0300

    Main

--- 1 - Pacotes/auxiliar/auxiliar.go
+++ 1 - Pacotes/auxiliar/auxiliar.go
@@ -2,7 +2,7 @@ package auxiliar
 
 import "fmt"
 
-// Escrever registra uma mensagem na tela Validacao
+// Escrever registra uma mensagem na tela validacao
 func Escrever() {
 	fmt.Println("Escrevendo do arquivo auxiliar")
 	escrever2()
