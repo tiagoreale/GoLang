1d0b0021b74ee8df2b4893f330e05b9a5521ba8a
tiagoreale <tiagoreale@live.com>
2023-03-08 16:42:13 -0300

    Main

--- 1 - Pacotes/main.go
+++ 1 - Pacotes/main.go
@@ -3,10 +3,15 @@ package main
 import (
 	"fmt"
 	"modulo/auxiliar"
+
+	"github.com/badoux/checkmail"
 )
 
 // Escrever registra uma mensagem na tela
 func main() {
 	fmt.Println("Escrevendo do arquivo main")
 	auxiliar.Escrever()
+
+	erro := checkmail.ValidateFormat("tiagopcreis@gmail.com")
+	fmt.Println(erro)
 }
