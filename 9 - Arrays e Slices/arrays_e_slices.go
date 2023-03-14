20b183b909a6cece392f36dfb3ba9c2d844a3dd4
Tiago Reale <tiagoreale@live.com>
2023-03-13 22:48:12 -0300

    Main

---
+++ 9 - Arrays e Slices/arrays_e_slices.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Arrays e Slices")
+
+	var array1 [5]string
+	array1[0] = "Posição 1"
+	fmt.Println(array1)
+
+	array2 := [5]string{"Posição 1", "Posição 2", "Posição 3", "Posição 4", "Posição 5"}
+	fmt.Println(array2)
+
+	array3 := [...]int{12345}
+	fmt.Println(array3)
+}
