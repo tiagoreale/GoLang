9d6d2d8a1ecb4cc69d09c14e0c5950dc03234996
tiagoreale <tiagoreale@live.com>
2023-03-22 17:35:15 -0300

    Main

--- 0 - Temp/temp.go
+++ 0 - Temp/temp.go
@@ -6,13 +6,12 @@ import (
 )
 
 func main() {
-	out, err := exec.Command("terminal", "/C", "tree").Output()
+	cmd := exec.Command("ls", "-la", "snmpwalk -v2c -c public 172.16.113.230 .1.3.6.1.4.1.3709.3.6.2.1.1.3")
+	out, err := cmd.CombinedOutput()
 	if err != nil {
-		fmt.Println("Error: ", err)
-		return
+		fmt.Println(err)
 	}
-
-	fmt.Println(string(out))
+	fmt.Printf("%s\n", out)
 }
 
 /*
@@ -43,4 +42,21 @@ Sim, é possível aplicar um comando MS-DOS em Go.
 Para fazer isso, você precisa usar o pacote "os/exec" do Go. Esse pacote fornece APIs para executar comandos do sistema operacional, incluindo comandos MS-DOS.
 
 Aqui está um exemplo de código que executa o comando MS-DOS "tree":
+
+package main
+
+import (
+	"fmt"
+	"os/exec"
+)
+
+func main() {
+	cmd := exec.Command("ls", "-la")
+	out, err := cmd.CombinedOutput()
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Printf("%s\n", out)
+}
+
 */
