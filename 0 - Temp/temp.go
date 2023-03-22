386969f3d1fdb7a0b86e1955235c8200cc73c216
tiagoreale <tiagoreale@live.com>
2023-03-22 17:20:47 -0300

    Main

--- 0 - Temp/temp.go
+++ 0 - Temp/temp.go
@@ -6,7 +6,7 @@ import (
 )
 
 func main() {
-	out, err := exec.Command("cmd", "/C", "tree", "snmpwalk -v2c -c public 172.16.113.230 .1.3.6.1.4.1.3709.3.6.2.1.1.3").Output()
+	out, err := exec.Command("cmd", "/C", "tree").Output()
 	if err != nil {
 		fmt.Println("Error: ", err)
 		return
