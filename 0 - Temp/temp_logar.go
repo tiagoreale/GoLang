cd2502efac67a6b3919f407c45dcf0dd485ded1c
tiagoreale <tiagoreale@live.com>
2023-03-25 12:17:29 -0300

    Main

--- 0 - Temp/temp_logar.go
+++ 0 - Temp/temp_logar.go
@@ -6,14 +6,14 @@ import (
 )
 
 func main() {
-	// Create a Cmd struct that runs "echo hello; date; pwd" command
+	// Ajustes de integração bash mais asssociações de comandos
 	cmd := exec.Command("bash", "-c", "echo hello; date; pwd; snmpwalk -v2c -c public 172.16.113.230 .1.3.6.1.4.1.3709.3.6.2.1.1.22")
-	// Call Output method to get the result as a byte slice
+	// Chamada do método Output para obter o resultado como uma fatia de byte
 	out, err := cmd.Output()
-	// Check for errors
+	// Verificação da existência de erros
 	if err != nil {
 		fmt.Println(err)
 	}
-	// Convert the byte slice to a string and print it
+	// Converção da fatia de byte em uma string pronta para impressão
 	fmt.Println(string(out))
 }
