bd55d14dcdcd95eba8945afc8194f21dcf2c608b
tiagoreale <tiagoreale@live.com>
2023-04-05 00:03:12 -0300

    Main

---
+++ 21 - HTTP/http.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func home(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Olá Mundo!"))
+}
+
+func usuarios(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Carrregar página de usuários!"))
+}
+
+func main() {
+	// HTTP É UM PROTOCOLO DE COMUNICAÇÃO - BASE DA COMUNICAÇÃO WEB
+
+	// CLIENTE (FAZ REQUISIÇÃO) - SERVIDOR (PROCESSA REQUISIÇÃO E ENVIA RESPOSTA)
+
+	// Request - Response
+
+	// Rotas
+	// URI - Identificador do Recurso
+	// Método - GET, POST, PUT, DELETE
+
+	http.HandleFunc("/home", home)
+	http.HandleFunc("/usuarios", usuarios)
+
+	log.Fatal(http.ListenAndServe(":5000", nil))
+}
