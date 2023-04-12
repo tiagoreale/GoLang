24ace719870a5ec695fe656e361dd93ec505c092
tiagoreale <tiagoreale@live.com>
2023-04-12 15:05:04 -0300

    Main

---
+++ 22 - HTML/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"text/template"
+)
+
+var templates *template.Template
+
+type usuario struct {
+	Nome  string
+	Email string
+}
+
+func main() {
+
+	templates = template.Must(template.ParseGlob("*.html"))
+
+	http.HandleFunc("/home", func(w http.ResponseWriter, r *http.Request) {
+
+		u := usuario{"João", "joao.pedro@gmail.com"}
+
+		templates.ExecuteTemplate(w, "home.html", u)
+	})
+
+	fmt.Println("Escutando na porta 5000")
+	log.Fatal(http.ListenAndServe(":5000", nil))
+}
