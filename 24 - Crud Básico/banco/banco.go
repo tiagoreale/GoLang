9fce5bdbab1a86bc690242d3aff52a2b2b17425e
tiagoreale <tiagoreale@live.com>
2023-04-27 15:56:56 -0300

    Main

--- 24 - Crud Básico/banco/banco.go
+++ 24 - Crud Básico/banco/banco.go
@@ -1,3 +1,23 @@
 package banco
 
-func Conectar() (*) 
+import (
+	"database/sql"
+
+	_ "github.com/gorilla/mux" // Driver de conexão com o MySQL
+)
+
+// Conectar abre a conexão com o banco de dados
+func Conectar() (*sql.DB, error) {
+	stringConexao := "golang:tech@2025@/devbook?charset=utf8&parseTime=True&loc=Local"
+
+	db, erro := sql.Open("mysql", stringConexao)
+	if erro != nil {
+		return nil, erro
+	}
+
+	if erro = db.Ping(); erro != nil {
+		return nil, erro
+	}
+
+	return db, nil
+}
--- 24 - Crud Básico/main.go
+++ 24 - Crud Básico/main.go
@@ -1,6 +1,19 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
 func main() {
 	// SIGNIFICADO DO CRUD - CREATE, READ, UPDATE, DELETE
+
+	router := mux.NewRouter()
+
+	fmt.Println("Escutando na porta 5000")
+	log.Fatal(http.ListenAndServe(":5000", router))
 
 }
