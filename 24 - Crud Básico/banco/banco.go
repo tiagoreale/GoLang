002a58577be1070f016a1f090bb5d29b794b83e6
tiagoreale <72996607+tiagoreale@users.noreply.github.com>
2023-05-11 10:21:16 -0300

    1-11.05.23(24)
    1-11.05.23(24)

--- 24 - Crud Básico/banco/banco.go
+++ 24 - Crud Básico/banco/banco.go
@@ -8,7 +8,7 @@ import (
 
 // Conectar abre a conexão com o banco de dados
 func Conectar() (*sql.DB, error) {
-	stringConexao := "golang:tech@2025@/devbook?charset=utf8&parseTime=True&loc=Local"
+	stringConexao := "golang:tech@@2025/devbook?charset=utf8&parseTime=True&loc=Local"
 
 	db, erro := sql.Open("mysql", stringConexao)
 	if erro != nil {
