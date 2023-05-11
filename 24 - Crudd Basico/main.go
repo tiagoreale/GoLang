dea48e37f46890664d4ca70e036f62a3e4beb1df
tiagoreale <72996607+tiagoreale@users.noreply.github.com>
2023-05-11 13:44:01 -0300

    Passo 24

---
+++ 24 - Crudd Basico/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"crud/servidor"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	// SIGNIFICADO DO CRUD - CREATE, READ, UPDATE, DELETE
+
+	// CREATE - POST
+	// READ - GET
+	// UPDATE - PUT
+	// DELETE - DELETE
+
+	router := mux.NewRouter()
+	router.HandleFunc("/usuarios", servidor.CriarUsuario).Methods(http.MethodPost)
+
+	fmt.Println("Escutando na porta 5000")
+	log.Fatal(http.ListenAndServe(":5000", router))
+
+}
